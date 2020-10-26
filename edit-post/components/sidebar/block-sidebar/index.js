893fda73cbf8df3f67bda83781e9ac335f3c4c11
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 03:42:41 +0900

    Initialize add/general-sidebar-exitensibility

---
+++ edit-post/components/sidebar/block-sidebar/index.js
@@ -0,0 +1,33 @@
+/**
+ * WordPress dependencies
+ */
+import { Panel, PanelBody } from '@wordpress/components';
+import { BlockInspector } from '@wordpress/editor';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal Dependencies
+ */
+import './style.scss';
+import SettingsHeader from '../settings-header';
+import SidebarExtender from '../sidebar-extender';
+import Sidebar from '../';
+
+const SIDEBAR_NAME = 'edit-post/block';
+
+const BlockSidebar = () => (
+	<Sidebar
+		name={ SIDEBAR_NAME }
+		label={ __( 'Editor settings' ) }
+	>
+		<SettingsHeader sidebarName={ SIDEBAR_NAME } />
+		<Panel>
+			<PanelBody className="edit-post-block-sidebar__panel">
+				<BlockInspector />
+			</PanelBody>
+			<SidebarExtender.Slot />
+		</Panel>
+	</Sidebar>
+);
+
+export default BlockSidebar;
