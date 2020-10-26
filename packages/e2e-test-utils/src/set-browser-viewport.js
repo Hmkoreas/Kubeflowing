3a79cddacd0e2afcd70351d3abb6addb3e35cece
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 04:02:29 +0900

    Initialize add/template-part-block

---
+++ packages/e2e-test-utils/src/set-browser-viewport.js
@@ -0,0 +1,20 @@
+/**
+ * Internal dependencies
+ */
+import { waitForWindowDimensions } from './wait-for-window-dimensions';
+
+/**
+ * Sets browser viewport to specified type.
+ *
+ * @param {string} type String to represent dimensions type; can be either small or large.
+ */
+export async function setBrowserViewport( type ) {
+	const allowedDimensions = {
+		large: { width: 960, height: 700 },
+		medium: { width: 768, height: 700 },
+		small: { width: 600, height: 700 },
+	};
+	const currentDimension = allowedDimensions[ type ];
+	await page.setViewport( currentDimension );
+	await waitForWindowDimensions( currentDimension.width, currentDimension.height );
+}
