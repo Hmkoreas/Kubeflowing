46a60f0f4ed23a7259d967b92c8f28c87c69d321
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 03:51:17 +0900

    Initialize try/element-queries-playground

---
+++ packages/block-editor/src/utils/transform-media-queries/index.js
@@ -0,0 +1,105 @@
+/**
+ * External dependencies
+ */
+import {
+	filter,
+	get,
+	map,
+	some,
+} from 'lodash';
+import ElementQueries from 'css-element-queries/src/ElementQueries';
+
+/**
+ * Internal dependencies
+ */
+import traverse from '../transform-styles/traverse';
+import wrap from '../transform-styles/transforms/wrap';
+
+function parseMediaQueryRule( rule ) {
+	// Make sure there are no multiple media rules.
+	if ( ! rule.media || rule.media.length !== 1 ) {
+		return null;
+	}
+
+	const mediaQueryCondition = rule.media[ 0 ];
+	// Verify if it is a simple media query that could be transformed into an element query.
+	const mediaMatches = mediaQueryCondition.match( /^\(((min|max)-(width|height)):([^\(]*?)\)$/ );
+	if ( ! mediaMatches || ! mediaMatches[ 1 ] || ! mediaMatches[ 4 ] ) {
+		return null;
+	}
+	return {
+		property: mediaMatches[ 1 ].trim(),
+		value: mediaMatches[ 4 ].trim(),
+	};
+}
+
+function getStyleSheetsThatMatchPaths( partialPaths ) {
+	return filter(
+		get( window, [ 'document', 'styleSheets' ], [] ),
+		( styleSheet ) => {
+			return (
+				styleSheet.href &&
+				some(
+					partialPaths,
+					( partialPath ) => {
+						return styleSheet.href.includes( partialPath );
+					}
+				)
+			);
+		}
+	);
+}
+
+function getMediaQueryInnerText( rule ) {
+	return map(
+		rule.cssRules,
+		( { cssText } ) => ( cssText )
+	).join( '\n' );
+}
+
+function getTransformedMediaQuery( rule ) {
+	const parsedMediaQuery = parseMediaQueryRule( rule );
+	if ( ! parsedMediaQuery ) {
+		return;
+	}
+	return traverse(
+		getMediaQueryInnerText( rule ),
+		wrap( `.editor-styles-wrapper[${ parsedMediaQuery.property }~="${ parsedMediaQuery.value }"]` )
+	);
+}
+
+export default function transformMediaQueries( partialPaths ) {
+	if ( ! window || ! window.document ) {
+		return;
+	}
+
+	const styleSheets = getStyleSheetsThatMatchPaths( partialPaths );
+
+	const rulesToProcess = [];
+
+	styleSheets.forEach(
+		( styleSheet ) => {
+			for ( let i = 0; i < styleSheet.rules.length; ) {
+				const rule = styleSheet.rules[ i ];
+				const transformedMediaQuery = getTransformedMediaQuery( rule );
+				if ( transformedMediaQuery ) {
+					rulesToProcess.push( transformedMediaQuery );
+					styleSheet.removeRule( i );
+				} else {
+					++i;
+				}
+			}
+		}
+	);
+	if ( ! rulesToProcess.length ) {
+		return;
+	}
+
+	const elementQueriesCode = rulesToProcess.join( '\n' );
+
+	const node = document.createElement( 'style' );
+	node.innerHTML = elementQueriesCode;
+	document.body.appendChild( node );
+	ElementQueries.listen();
+	ElementQueries.init();
+}
---
+++ packages/edit-widgets/src/components/widget-areas/index.js
@@ -0,0 +1,53 @@
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useMemo, useState } from '@wordpress/element';
+import { compose } from '@wordpress/compose';
+import { withSelect } from '@wordpress/data';
+import { transformMediaQueries } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import WidgetArea from '../widget-area';
+
+function WidgetAreas( { areas, blockEditorSettings } ) {
+	useEffect( () => {
+		// Todo: The partial paths should be a setting that includes styles added by the plugins.
+		transformMediaQueries( [
+			'block-editor/style.css',
+			'block-library/style.css',
+			'block-library/theme.css',
+			'block-library/editor.css',
+			'format-library/style.css',
+		] );
+	}, [] );
+	const [ selectedArea, setSelectedArea ] = useState( 0 );
+	const onBlockSelectedInArea = useMemo(
+		() => areas.map( ( value, index ) => ( () => {
+			setSelectedArea( index );
+		} ) ),
+		[ areas, setSelectedArea ]
+	);
+
+	return areas.map( ( { id }, index ) => (
+		<WidgetArea
+			isSelectedArea={ index === selectedArea }
+			onBlockSelected={ onBlockSelectedInArea[ index ] }
+			blockEditorSettings={ blockEditorSettings }
+			key={ id }
+			id={ id }
+			initialOpen={ index === 0 }
+		/>
+	) );
+}
+
+export default compose( [
+	withSelect( ( select ) => {
+		const { getWidgetAreas } = select( 'core/edit-widgets' );
+		const areas = getWidgetAreas();
+		return {
+			areas,
+		};
+	} ),
+] )( WidgetAreas );
