1221537f95fd2c7349869776214283d117a95c1a
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 03:46:20 +0900

    Initialize release/5.7

---
+++ packages/e2e-tests/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	...require( '@wordpress/scripts/config/jest-e2e.config' ),
+	setupFiles: [
+		'<rootDir>/config/gutenberg-phase.js',
+	],
+	setupFilesAfterEnv: [
+		'<rootDir>/config/setup-test-framework.js',
+		'@wordpress/jest-console',
+		'@wordpress/jest-puppeteer-axe',
+		'expect-puppeteer',
+	],
+};
