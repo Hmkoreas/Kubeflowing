d49d6f5bcdffe6b4e13677129a226a49811623dd
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 03:45:18 +0900

    Initialize add/document-approach-to-have-drafts-as-parent-page

---
+++ packages/block-library/src/button/index.js
@@ -0,0 +1,121 @@
+/**
+ * External dependencies
+ */
+import { omit, pick } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { __, _x } from '@wordpress/i18n';
+import { RichText } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import edit from './edit';
+import icon from './icon';
+import metadata from './block.json';
+import save from './save';
+
+const { name, attributes: blockAttributes } = metadata;
+
+export { metadata, name };
+
+const colorsMigration = ( attributes ) => {
+	return omit( {
+		...attributes,
+		customTextColor: attributes.textColor && '#' === attributes.textColor[ 0 ] ? attributes.textColor : undefined,
+		customBackgroundColor: attributes.color && '#' === attributes.color[ 0 ] ? attributes.color : undefined,
+	}, [ 'color', 'textColor' ] );
+};
+
+export const settings = {
+	title: __( 'Button' ),
+	description: __( 'Prompt visitors to take action with a button-style link.' ),
+	icon,
+	keywords: [ __( 'link' ) ],
+	supports: {
+		align: true,
+		alignWide: false,
+	},
+	styles: [
+		{ name: 'default', label: _x( 'Default', 'block style' ), isDefault: true },
+		{ name: 'outline', label: __( 'Outline' ) },
+		{ name: 'squared', label: _x( 'Squared', 'block style' ) },
+	],
+	edit,
+	save,
+	deprecated: [ {
+		attributes: {
+			...pick( blockAttributes, [ 'url', 'title', 'text' ] ),
+			color: {
+				type: 'string',
+			},
+			textColor: {
+				type: 'string',
+			},
+			align: {
+				type: 'string',
+				default: 'none',
+			},
+		},
+
+		save( { attributes } ) {
+			const { url, text, title, align, color, textColor } = attributes;
+
+			const buttonStyle = {
+				backgroundColor: color,
+				color: textColor,
+			};
+
+			const linkClass = 'wp-block-button__link';
+
+			return (
+				<div className={ `align${ align }` }>
+					<RichText.Content
+						tagName="a"
+						className={ linkClass }
+						href={ url }
+						title={ title }
+						style={ buttonStyle }
+						value={ text }
+					/>
+				</div>
+			);
+		},
+		migrate: colorsMigration,
+	},
+	{
+		attributes: {
+			...pick( blockAttributes, [ 'url', 'title', 'text' ] ),
+			color: {
+				type: 'string',
+			},
+			textColor: {
+				type: 'string',
+			},
+			align: {
+				type: 'string',
+				default: 'none',
+			},
+		},
+
+		save( { attributes } ) {
+			const { url, text, title, align, color, textColor } = attributes;
+
+			return (
+				<div className={ `align${ align }` } style={ { backgroundColor: color } }>
+					<RichText.Content
+						tagName="a"
+						href={ url }
+						title={ title }
+						style={ { color: textColor } }
+						value={ text }
+					/>
+				</div>
+			);
+		},
+		migrate: colorsMigration,
+	},
+	],
+};
