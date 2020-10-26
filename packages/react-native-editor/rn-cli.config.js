f023704e5cf15be05ebe0703605a7c1314778f88
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 04:04:39 +0900

    Initialize gutenberg-mobile-monorepo

---
+++ packages/react-native-editor/rn-cli.config.js
@@ -0,0 +1,21 @@
+/** @format */
+const path = require( 'path' );
+// Blacklist the nested GB filetree so modules are not resolved in duplicates,
+//  both in the nested directory and the parent directory.
+
+const enm = require( './extra-node-modules.config.js' );
+
+module.exports = {
+	projectRoot: path.resolve( __dirname, './' ),
+	resolver: {
+		extraNodeModules: enm,
+		sourceExts: [ 'js', 'json', 'scss', 'sass' ],
+	},
+	transformer: {
+		babelTransformerPath: require.resolve( './sass-transformer.js' ),
+	},
+	watchFolders: [
+		path.resolve( __dirname, '../' ),
+		path.resolve( __dirname, '../../node_modules' ),
+	],
+};
---
+++ test/unit/jest.config.js
@@ -0,0 +1,33 @@
+/**
+ * External dependencies
+ */
+const glob = require( 'glob' ).sync;
+
+// Finds all packages which are transpiled with Babel to force Jest to use their source code.
+const transpiledPackageNames = glob( 'packages/*/src/index.js' )
+	.map( ( fileName ) => fileName.split( '/' )[ 1 ] );
+
+module.exports = {
+	rootDir: '../../',
+	moduleNameMapper: {
+		[ `@wordpress\\/(${ transpiledPackageNames.join( '|' ) })$` ]: 'packages/$1/src',
+	},
+	preset: '@wordpress/jest-preset-default',
+	setupFiles: [
+		'<rootDir>/test/unit/config/gutenberg-phase.js',
+	],
+	testURL: 'http://localhost',
+	testPathIgnorePatterns: [
+		'/\.git/',
+		'/node_modules/',
+		'/packages/e2e-tests',
+		'/wordpress/',
+		'<rootDir>/.*/build/',
+		'<rootDir>/.*/build-module/',
+		'<rootDir>/.+\.native\.js$',
+		'/packages/react-native-editor',
+	],
+	transform: {
+		'^.+\\.[jt]sx?$': '<rootDir>/test/unit/scripts/babel-transformer.js',
+	},
+};
