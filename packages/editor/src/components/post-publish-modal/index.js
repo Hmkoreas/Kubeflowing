0ea2fd10484510906e131c6d6913e88404e86781
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 03:53:16 +0900

    Initialize try/publish-via-full-screen-modal

---
+++ packages/editor/src/components/post-publish-modal/index.js
@@ -0,0 +1,174 @@
+/**
+ * External dependencies
+ */
+import { get, omit } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { __, sprintf } from '@wordpress/i18n';
+import { Component } from '@wordpress/element';
+import {
+	Spinner,
+	CheckboxControl,
+	withFocusReturn,
+	withConstrainedTabbing,
+	Modal,
+	Button,
+} from '@wordpress/components';
+import { withSelect, withDispatch } from '@wordpress/data';
+import { compose } from '@wordpress/compose';
+
+/**
+ * Internal dependencies
+ */
+import PostPublishButton from '../post-publish-button';
+import PostPublishModalPrepublish from './prepublish';
+import PostPublishModalPostpublish from './postpublish';
+
+export class PostPublishModal extends Component {
+	constructor() {
+		super( ...arguments );
+		this.onSubmit = this.onSubmit.bind( this );
+	}
+
+	onSubmit() {
+		const { onClose, hasPublishAction, isPostTypeViewable } = this.props;
+		if ( ! hasPublishAction || ! isPostTypeViewable ) {
+			onClose();
+		}
+	}
+
+	render() {
+		const {
+			forceIsDirty,
+			forceIsSaving,
+			isBeingScheduled,
+			isPublished,
+			isPublishSidebarEnabled,
+			isScheduled,
+			isSaving,
+			onClose,
+			onTogglePublishSidebar,
+			PostPublishExtension,
+			PrePublishExtension,
+			hasPublishAction,
+			postLabel,
+			...additionalProps
+		} = this.props;
+		const propsForModal = omit( additionalProps, [ 'hasPublishAction', 'isDirty', 'isPostTypeViewable' ] );
+		const isPublishedOrScheduled = isPublished || ( isScheduled && isBeingScheduled );
+		const isPrePublish = ! isPublishedOrScheduled || isSaving;
+		const isPostPublish = isPublishedOrScheduled && ! isSaving;
+
+		let modalTitle;
+		if ( isSaving ) {
+			modalTitle = isScheduled ? __( 'Scheduling' ) : __( 'Publishing' );
+		} else if ( isPrePublish ) {
+			if ( ! hasPublishAction ) {
+				modalTitle = __( 'Ready to submit for review?' );
+			} else if ( isBeingScheduled ) {
+				modalTitle = __( 'Ready to schedule?' );
+			} else {
+				modalTitle = __( 'Ready to publish?' );
+			}
+		} else if ( isScheduled ) {
+			modalTitle = sprintf(
+				/* translators: %s: post type singular name */
+				__( '%s Scheduled!' ), postLabel
+			);
+		} else {
+			modalTitle = sprintf(
+				/* translators: %s: post type singular name */
+				__( '%s Published!' ), postLabel
+			);
+		}
+		return (
+			<Modal
+				className="editor-post-publish-modal"
+				title={ modalTitle }
+				closeLabel={ __( 'Close' ) }
+				onRequestClose={ onClose }
+			>
+				<div className="editor-post-publish-modal__content" { ...propsForModal }>
+					{ isPrePublish && (
+						isSaving ?
+							<div className="editor-post-publish-modal__publishing-animation">
+								<Spinner />
+							</div> :
+							<>
+								<PostPublishModalPrepublish>
+									{ PrePublishExtension && <PrePublishExtension /> }
+								</PostPublishModalPrepublish>
+								<CheckboxControl
+									label={ __( 'Always show these pre-publish checks.' ) }
+									checked={ isPublishSidebarEnabled }
+									onChange={ onTogglePublishSidebar }
+								/>
+								<div className="editor-post-publish-modal__prepublish-controls">
+									<Button
+										isLink
+										className="editor-post-publish-modal__prepublish-cancel-button"
+										onClick={ onClose }
+									>
+										{ __( 'Cancel' ) }
+									</Button>
+									<PostPublishButton isLarge className="editor-post-publish-modal__content-publish-button" focusOnMount={ true } onSubmit={ this.onSubmit } forceIsDirty={ forceIsDirty } forceIsSaving={ forceIsSaving } />
+								</div>
+							</>
+					) }
+
+					{ isPostPublish && (
+						<PostPublishModalPostpublish focusOnMount={ true } >
+							{ PostPublishExtension && <PostPublishExtension /> }
+						</PostPublishModalPostpublish>
+					) }
+				</div>
+			</Modal>
+		);
+	}
+}
+
+export default compose( [
+	withSelect( ( select ) => {
+		const { getPostType } = select( 'core' );
+		const {
+			getCurrentPost,
+			getEditedPostAttribute,
+			isCurrentPostPublished,
+			isCurrentPostScheduled,
+			isEditedPostBeingScheduled,
+			isEditedPostDirty,
+			isSavingPost,
+		} = select( 'core/editor' );
+		const { isPublishSidebarEnabled } = select( 'core/editor' );
+		const postType = getPostType( getEditedPostAttribute( 'type' ) );
+
+		return {
+			hasPublishAction: get( getCurrentPost(), [ '_links', 'wp:action-publish' ], false ),
+			isPostTypeViewable: get( postType, [ 'viewable' ], false ),
+			isBeingScheduled: isEditedPostBeingScheduled(),
+			isDirty: isEditedPostDirty(),
+			isPublished: isCurrentPostPublished(),
+			isPublishSidebarEnabled: isPublishSidebarEnabled(),
+			isSaving: isSavingPost(),
+			isScheduled: isCurrentPostScheduled(),
+			postLabel: get( postType, [ 'labels', 'singular_name' ] ),
+		};
+	} ),
+	withDispatch( ( dispatch, { isPublishSidebarEnabled } ) => {
+		const { disablePublishSidebar, enablePublishSidebar } = dispatch( 'core/editor' );
+
+		return {
+			onTogglePublishSidebar: ( ) => {
+				if ( isPublishSidebarEnabled ) {
+					disablePublishSidebar();
+				} else {
+					enablePublishSidebar();
+				}
+			},
+		};
+	} ),
+	withFocusReturn,
+	withConstrainedTabbing,
+] )( PostPublishModal );
---
+++ packages/editor/src/components/post-publish-modal/prepublish.js
@@ -0,0 +1,103 @@
+/**
+ * External dependencies
+ */
+import { get, find } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { __, sprintf } from '@wordpress/i18n';
+import { withSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import PostVisibilityLabel from '../post-visibility/label';
+import PostScheduleLabel from '../post-schedule/label';
+import { visibilityOptions } from '../post-visibility/utils';
+
+function PostPublishModalPrepublish( {
+	hasPublishAction,
+	publishDate,
+	isFloating,
+	visibility,
+	children,
+	postType,
+} ) {
+	const postLabel = get( postType, [ 'labels', 'singular_name' ] );
+	let prePublishBodyText,
+		prePublishDateText;
+
+	if ( ! hasPublishAction ) {
+		prePublishBodyText = __( 'When you’re ready, submit your work for review, and an Editor will be able to approve it for you.' );
+	} else {
+		prePublishBodyText = sprintf(
+			/* translators: %s: post type singular name */
+			__( 'Double-check your settings, then publish your %s.' ), postLabel
+		);
+	}
+
+	if ( isFloating ) {
+		prePublishDateText = __( 'Your work will be published right now.' );
+	} else if ( new Date( publishDate ) < new Date() ) {
+		prePublishDateText = __( 'Your work will be published right now and back-dated.' );
+	} else {
+		prePublishDateText = __( 'Your work will be published at the specified date and time.' );
+	}
+
+	const getVisibilityInfo = () => find( visibilityOptions, { value: visibility } ).info;
+
+	return (
+		<div className="editor-post-publish-modal__prepublish">
+			<p>{ prePublishBodyText }</p>
+			{ hasPublishAction && (
+				<>
+					<div className="editor-post-publish-modal-panel">
+						<div className="editor-post-publish-modal-panel__heading">
+							{ __( 'Publish:' ) }
+						</div>
+						<div className="editor-post-publish-modal-panel__value">
+							<PostScheduleLabel />
+						</div>
+						<div className="editor-post-publish-modal__detail">
+							{ prePublishDateText }
+						</div>
+					</div>
+					<div className="editor-post-publish-modal-panel">
+						<div className="editor-post-publish-modal-panel__heading">
+							{ __( 'Visibility:' ) }
+						</div>
+						<div className="editor-post-publish-modal-panel__value">
+							<PostVisibilityLabel />
+						</div>
+						<div className="editor-post-publish-modal__detail">
+							{ getVisibilityInfo( visibility ) }
+						</div>
+					</div>
+					<div className="editor-post-publish-modal-panel">
+						{ children }
+					</div>
+				</>
+			) }
+		</div>
+	);
+}
+
+export default withSelect(
+	( select ) => {
+		const {
+			getCurrentPost,
+			getEditedPostVisibility,
+			getEditedPostAttribute,
+			isEditedPostDateFloating,
+		} = select( 'core/editor' );
+		const { getPostType } = select( 'core' );
+		return {
+			hasPublishAction: get( getCurrentPost(), [ '_links', 'wp:action-publish' ], false ),
+			visibility: getEditedPostVisibility(),
+			publishDate: getEditedPostAttribute( 'date' ),
+			isFloating: isEditedPostDateFloating(),
+			postType: getPostType( getEditedPostAttribute( 'type' ) ),
+		};
+	}
+)( PostPublishModalPrepublish );
