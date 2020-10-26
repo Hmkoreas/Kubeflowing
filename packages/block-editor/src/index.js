3a56354356a3a4fcd8292f8572c55e76a320f03a
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 03:50:36 +0900

    Initialize try/xp-galleries

---
+++ packages/block-editor/src/index.js
@@ -0,0 +1,17 @@
+/**
+ * WordPress dependencies
+ */
+import '@wordpress/blocks';
+import '@wordpress/rich-text';
+import '@wordpress/viewport';
+
+/**
+ * Internal dependencies
+ */
+import './store';
+import './hooks';
+
+export * from './components';
+export * from './utils';
+export { storeConfig } from './store';
+export { SETTINGS_DEFAULTS } from './store/defaults';
