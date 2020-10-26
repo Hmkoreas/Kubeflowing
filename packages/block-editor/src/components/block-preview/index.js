fe2b3123e30973361668b6fddf6d1da71e6a5c46
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 03:49:45 +0900

    Initialize add/token-input-commit-on-blur

---
+++ packages/block-editor/src/components/block-preview/index.js
@@ -0,0 +1,56 @@
+/**
+ * External dependencies
+ */
+import { castArray } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Disabled } from '@wordpress/components';
+import { withSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import BlockEditorProvider from '../provider';
+import BlockList from '../block-list';
+
+/**
+ * Block Preview Component: It renders a preview given a block name and attributes.
+ *
+ * @param {Object} props Component props.
+ *
+ * @return {WPElement} Rendered element.
+ */
+function BlockPreview( props ) {
+	return (
+		<div className="editor-block-preview block-editor-block-preview">
+			<div className="editor-block-preview__title block-editor-block-preview__title">{ __( 'Preview' ) }</div>
+			<BlockPreviewContent { ...props } />
+		</div>
+	);
+}
+
+export function BlockPreviewContent( { blocks, settings } ) {
+	if ( ! blocks ) {
+		return null;
+	}
+
+	return (
+		<Disabled className="editor-block-preview__content block-editor-block-preview__content editor-styles-wrapper" aria-hidden>
+			<BlockEditorProvider
+				value={ castArray( blocks ) }
+				settings={ settings }
+			>
+				<BlockList />
+			</BlockEditorProvider>
+		</Disabled>
+	);
+}
+
+export default withSelect( ( select ) => {
+	return {
+		settings: select( 'core/block-editor' ).getSettings(),
+	};
+} )( BlockPreview );
---
+++ packages/data/src/factory.js
@@ -0,0 +1,50 @@
+/**
+ * Internal dependencies
+ */
+import defaultRegistry from './default-registry';
+
+/**
+ * @typedef {import('./registry').WPDataRegistry} WPDataRegistry
+ */
+
+/**
+ * Mark a selector as a registry selector.
+ *
+ * @param {function} registrySelector Function receiving a registry object and returning a state selector.
+ *
+ * @return {function} marked registry selector.
+ */
+export function createRegistrySelector( registrySelector ) {
+	const selector = ( ...args ) => registrySelector( selector.registry.select )( ...args );
+
+	/**
+	 * Flag indicating to selector registration mapping that the selector should
+	 * be mapped as a registry selector.
+	 *
+	 * @type {boolean}
+	 */
+	selector.isRegistrySelector = true;
+
+	/**
+	 * Registry on which to call `select`, stubbed for non-standard usage to
+	 * use the default registry.
+	 *
+	 * @type {WPDataRegistry}
+	 */
+	selector.registry = defaultRegistry;
+
+	return selector;
+}
+
+/**
+ * Mark a control as a registry control.
+ *
+ * @param {function} registryControl Function receiving a registry object and returning a control.
+ *
+ * @return {function} marked registry control.
+ */
+export function createRegistryControl( registryControl ) {
+	registryControl.isRegistryControl = true;
+
+	return registryControl;
+}
