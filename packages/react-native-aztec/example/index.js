4a0ec455a914c9e4ce6aa1efe3e1a2c8846f9a4c
Hmkorea <58475588+mgr2178@users.noreply.github.com>
2020-10-27 03:59:35 +0900

    Initialize import-gutenberg-mobile

---
+++ packages/react-native-aztec/example/index.js
@@ -0,0 +1,10 @@
+/**
+ * External dependencies
+ */
+import { AppRegistry } from 'react-native';
+/**
+ * Internal dependencies
+ */
+import example from './App';
+
+AppRegistry.registerComponent( 'example', () => example );
